3d94ef30f0f3cebe640376e11445f1211e410358
Arseny Stanley <120415036+arsenystanley@users.noreply.github.com>
2023-03-19 20:39:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const btnEl = document.getElementById("btn-el")
+const resetEl = document.getElementById("reset-el")
+const inputEl = document.getElementById("input-el")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+let meterToFeet = inputEl.value * 3.281
+let literToGallon = inputEl.value * 0.264
+let kilogramsToPound = inputEl.value * 2.204
+let feetToMeter = inputEl.value / 3.281
+let gallonToLiter = inputEl.value / 0.264
+let poundToKilograms = inputEl.value / 2.204
+
+function render(baseValue, foot, meter, gallon, liter, pound, kilogram) {
+    lengthEl.textContent = `${baseValue} meters = ${foot.toFixed(3)} feet | ${baseValue} feet = ${meter.toFixed(3)} meters`;
+    
+    volumeEl.textContent = `${baseValue} liters = ${gallon.toFixed(3)} gallons | ${baseValue} gallons = ${liter.toFixed(3)} liters`;
+    
+    massEl.textContent = `${baseValue} kilograms = ${pound.toFixed(3)} pounds | ${baseValue} pounds = ${kilogram.toFixed(3)} kilograms`
+}
+
+btnEl.addEventListener("click", function() {
+    let baseValue = Number(inputEl.value)
+    
+    if (!inputEl.value) {
+        alert("Please, add numbers to convert!");
+    } else if (isNaN(baseValue)) {
+        alert("Sorry, only numbers are accepted!");
+    } else {
+        let meterToFeet = baseValue * 3.281;
+        let literToGallon = baseValue * 0.264;
+        let kilogramsToPound = baseValue * 2.204;
+        let feetToMeter = baseValue / 3.281;
+        let gallonToLiter = baseValue / 0.264;
+        let poundToKilograms = baseValue / 2.204;
+        
+      render(inputEl.value, meterToFeet, feetToMeter, literToGallon, gallonToLiter, kilogramsToPound, poundToKilograms);
+    }
+
+})
+
+ resetEl.addEventListener("click", function() {
+    inputEl.value = "";
+    lengthEl.innerHTML = ` 0 meters = 0.000 feet | 0 feet = 0.000 meter`;
+    volumeEl.innerHTML = ` 0 litres = 0.000 gallons | 0 gallons = 0.000 litres`;
+    massEl.innerHTML = ` 0 kilos = 0.000 pounds | 0 pounds = 0.000 kilos`;
+ })
